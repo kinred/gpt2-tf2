715db862d008f603a1154356f791740038ec0f5a
kinred <toine@aime.info>
2020-01-15 11:20:17 +0100

    --epochs switch added to define how many epochs to train on data

--- train_gpt2.py
+++ train_gpt2.py
@@ -13,16 +13,16 @@ LOG_DIR = _ROOT + "/log"
 @click.option('--model-dir', type=str, default="./model", show_default=True, help="Directory to load model")
 @click.option('--data-dir', type=str, default="./data", show_default=True, help="training data directory")
 @click.option('--batch-size', type=int, default=16, show_default=True, help="batch size")
+@click.option('--epochs', type=int, default=5, show_default=True, help="num of epochs to train on data")
 @click.option('--learning-rate', type=float, default=0.001, show_default=True, help="learning rate")
 @click.option('--distributed', type=bool, default=False, show_default=True, help="distributed training")
 @click.option('--mxp', type=bool, default=False, show_default=True, help="enable mixed precission training")
-def train(model_dir, data_dir, batch_size=16, learning_rate=0.001, distributed=False, mxp=False):
+def train(model_dir, data_dir, batch_size=16, learning_rate=0.001, distributed=False, mxp=False, epochs=5):
     data_dir = os.path.abspath(data_dir)
     model_dir = os.path.abspath(model_dir)
     tf_records = glob.glob(data_dir + "/tf_records/*.tfrecord")
-    dataset = None
+    dataset = input_fn(tf_records, batch_size=batch_size, epoch=epochs)
     if distributed:
-        dataset = input_fn(tf_records, batch_size=batch_size)
         mirrored_strategy = tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1"])
         dataset = mirrored_strategy.experimental_distribute_dataset(dataset)
         with mirrored_strategy.scope():
@@ -33,7 +33,6 @@ def train(model_dir, data_dir, batch_size=16, learning_rate=0.001, distributed=F
 
         model.mirrored_strategy = mirrored_strategy
     else:
-        dataset = input_fn(tf_records, batch_size=batch_size)
         model = Gpt2.create_from_params(model_dir)
         model.create_optimizer(learning_rate=learning_rate, mixed_precission=mxp)
         model.create_checkpoint_manager(model_dir)
